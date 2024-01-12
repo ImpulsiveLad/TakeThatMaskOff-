fda30e2ff047a7cd7a9edd3e4a604f3aeb009265
ImpulsiveLad <92990441+ImpulsiveLad@users.noreply.github.com>
2024-01-11 22:35:41 -0500

    added the stuff and the things

---
+++ EnemyAIPatch.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using System.Linq;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace TakeTheMaskOff.Patches
+{
+    [HarmonyPatch(typeof(MaskedPlayerEnemy))]
+    internal class MaskedPlayerEnemy_Patches
+    {
+        [HarmonyPatch(nameof(MaskedPlayerEnemy.KillEnemy))]
+        [HarmonyPostfix]
+        internal static void DropMaskOnDeath(MaskedPlayerEnemy __instance)
+        {
+            UnMaskTheDeadBase.Instance.mls.LogInfo("HarmonyPatchOpened");
+            if (!NetworkManager.Singleton.IsServer) return;
+
+            var maskToSpawn = Random.value >= 0.5f ? GetTragedyItem() : GetComedyItem();
+
+            var spawnedMask = GameObject.Instantiate(maskToSpawn.spawnPrefab, __instance.transform.position + new Vector3(0, 2.5f, 0), Quaternion.identity);
+            spawnedMask.GetComponentInChildren<GrabbableObject>().fallTime = 0f;
+            spawnedMask.GetComponentInChildren<GrabbableObject>().SetScrapValue(100);
+            spawnedMask.GetComponentInChildren<NetworkObject>().Spawn();
+            UnMaskTheDeadBase.Instance.mls.LogInfo("Halfway there, the vars vared");
+            RoundManager.Instance.SyncScrapValuesClientRpc(new NetworkObjectReference[] { spawnedMask.GetComponent<NetworkObject>() }, new int[] { spawnedMask.GetComponent<GrabbableObject>().scrapValue });
+        }
+
+        private static Item _tragedyItem;
+        private static Item GetTragedyItem()
+        {
+            if (_tragedyItem == null)
+            {
+                _tragedyItem = StartOfRound.Instance.allItemsList.itemsList.First(i => i.name == "TragedyMask");
+            }
+            UnMaskTheDeadBase.Instance.mls.LogInfo("poggies");
+            return _tragedyItem;
+        }
+
+        private static Item _comedyItem;
+        private static Item GetComedyItem()
+        {
+            if (_comedyItem == null)
+            {
+                _comedyItem = StartOfRound.Instance.allItemsList.itemsList.First(i => i.name == "ComedyMask");
+            }
+            UnMaskTheDeadBase.Instance.mls.LogInfo("Check Check ALLLLLL Check");
+            return _comedyItem;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using TakeTheMaskOff.Patches;
+
+namespace TakeTheMaskOff
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class UnMaskTheDeadBase : BaseUnityPlugin
+    {
+        private const string modGUID = "impulse.TakeTheMaskOff";
+        private const string modName = "TakeTheMaskOff";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static UnMaskTheDeadBase Instance;
+
+        internal ManualLogSource mls; // Corrected here
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Time to de the sus"); // Corrected here
+
+            harmony.PatchAll(typeof(UnMaskTheDeadBase));
+            harmony.PatchAll(typeof(MaskedPlayerEnemy_Patches));
+        }
+    }
+}
