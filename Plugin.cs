ff63a561c93f05b4fe45978a12fec7f0c59ed757
ImpulsiveLad <92990441+ImpulsiveLad@users.noreply.github.com>
2024-01-15 17:55:13 -0500

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -12,7 +12,7 @@ namespace TakeTheMaskOff
     {
         private const string modGUID = "impulse.TakeTheMaskOff";
         private const string modName = "TakeTheMaskOff";
-        private const string modVersion = "1.1.0";
+        private const string modVersion = "1.3.0";
 
         private readonly Harmony harmony = new Harmony(modGUID);
 
@@ -31,7 +31,7 @@ namespace TakeTheMaskOff
             mls.LogInfo("Time to de the sus"); // Corrected here
             MaskValue = Config.Bind("General",      // The section under which the option is shown
                                     "MaskValue",  // The key of the configuration option in the configuration file
-                                    100, // The default value
+                                    52, // The default value
                                     "This value controls the value of the dropped masks"); // Description of the option to show in the config file
             harmony.PatchAll(typeof(UnMaskTheDeadBase));
             harmony.PatchAll(typeof(MaskedPlayerEnemy_Patches));
